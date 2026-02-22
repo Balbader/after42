12a6772daa66d69766275c05f38dcb0c5e1a9858
BASIL ALBADER <basilalbapro@gmail.com>
2026-02-22 12:12:21 +0100

    WIP

--- src/app/(logged-in)/challenge/create/page.tsx
+++ src/app/(logged-in)/challenge/create/page.tsx
@@ -1,7 +1,6 @@
-import { JobPostUploader } from '@/components/job-post-uploader';
+import { JobPostUploader } from '@/components/job-post/job-post-uploader';
 import { authController } from '@/bff/controllers/auth.controller';
 import { headers } from 'next/headers';
-import { JobPostList } from '@/components/job-post-list';
 
 export default async function CreateChallengePage() {
   const { user } = await authController.requireSession(await headers());
@@ -9,7 +8,6 @@ export default async function CreateChallengePage() {
   return (
     <div className='w-full min-w-0'>
       <JobPostUploader recruiterId={user?.id ?? ''} />
-      <JobPostList recruiterId={user?.id ?? ''} />
     </div>
   );
 }
